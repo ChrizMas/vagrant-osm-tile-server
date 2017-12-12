46c5539695a44d3b2476d9b23244aa68aedb951f
Chriz Mas <hanzzen@gmail.com>
2017-12-12 23:51:21 +0100

    Rewrite mapnik role to use default 16.04 libs

--- ansible/roles/mapnik/files/make-mapnik.sh
+++ ansible/roles/mapnik/files/make-mapnik.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cd ~/src
+test -d ~/src || mkdir  ~/src ; cd ~/src
 
 echo "export ENV vars"
 export CXX="clang++-3.8" && export CC="clang-3.8"
 
-echo "git clone https://github.com/mapnik/mapnik mapnik-3.x --depth 10"
+echo "git clone"
 git clone https://github.com/mapnik/mapnik mapnik-3.x --depth 10
 cd mapnik-3.x
 
@@ -15,7 +15,7 @@ git submodule update --init
 echo "source bootstrap.sh"
 source bootstrap.sh
 
-echo "/configure with EVN CXX=${CXX} CC=${CC}"
+echo "configure with EVN CXX=${CXX} CC=${CC}"
 ./configure CUSTOM_CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0" CXX=${CXX} CC=${CC}
 
 echo "make"
